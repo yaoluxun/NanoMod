775c2a71d43b90caca54511a617ed5fa6592e9ef
yaoluxun <yaoluxun2006@gmail.com>
2019-12-05 14:15:34 -0500

    Fix syntax error

--- bin/scripts/myDownSampling0.py
+++ bin/scripts/myDownSampling0.py
@@ -63,16 +63,16 @@ def mSimulate1(moptions):
       moresample = repeat_time
       if moresample>15: moresample = 15;
 
-      case_sample_num = int(moptions['CaseSize']*(1+moresample*0.02)
+      case_sample_num = int(moptions['CaseSize']*(1+moresample*0.02))
       if len(allcasekeys) > case_sample_num:
-          mcase_rand = np.random.choice(len(allcasekeys), case_sample_num), replace=False);
+          mcase_rand = np.random.choice(len(allcasekeys), case_sample_num), replace=False)
           mcase1 = {allcasekeys[x]:moptions['casefolder'][allcasekeys[x]] for x in mcase_rand}
       else:
           mcase1 =  moptions['casefolder']
 
-      cont_sample_num = int(moptions['CaseSize']*(1+moresample*0.02)
+      cont_sample_num = int(moptions['CaseSize']*(1+moresample*0.02))
       if len(allcontkeys) > cont_sample_num:
-          con_random = np.random.choice(len(allcontkeys), cont_sample_num), replace=False);
+          con_random = np.random.choice(len(allcontkeys), cont_sample_num), replace=False)
           mcon1 = {allcontkeys[x]:moptions['controlsample'][allcontkeys[x]] for x in con_random}
       else:
           mcon1 = moptions['controlsample']
